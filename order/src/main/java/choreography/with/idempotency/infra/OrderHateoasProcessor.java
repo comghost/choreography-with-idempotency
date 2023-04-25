97a63b5bf15c0efab305b13be0b34f05e6b16b4b
comghost <overpassion@gmail.com>
2023-04-25 15:27:01 +0900

    commit

---
+++ order/src/main/java/choreography/with/idempotency/infra/OrderHateoasProcessor.java
@@ -0,0 +1,17 @@
+package choreography.with.idempotency.infra;
+
+import choreography.with.idempotency.domain.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderHateoasProcessor
+    implements RepresentationModelProcessor<EntityModel<Order>> {
+
+    @Override
+    public EntityModel<Order> process(EntityModel<Order> model) {
+        return model;
+    }
+}
---
+++ product/src/main/java/choreography/with/idempotency/domain/StockIncreased.java
@@ -0,0 +1,26 @@
+package choreography.with.idempotency.domain;
+
+import choreography.with.idempotency.domain.*;
+import choreography.with.idempotency.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class StockIncreased extends AbstractEvent {
+
+    private Long id;
+    private String productName;
+    private String productImage;
+    private Integer stock;
+    private String orderId;
+    private String productId;
+
+    public StockIncreased(Inventory aggregate) {
+        super(aggregate);
+    }
+
+    public StockIncreased() {
+        super();
+    }
+}
